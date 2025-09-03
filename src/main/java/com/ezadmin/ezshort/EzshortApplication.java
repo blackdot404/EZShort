cb8eebee941b0e3604421e58cc532d35642b28f7
Edgar Andrade <blackstv11@gmail.com>
2025-09-03 09:59:37 -0300

    Projeto Iniciado

---
+++ src/main/java/com/ezadmin/ezshort/EzshortApplication.java
@@ -0,0 +1,13 @@
+package com.ezadmin.ezshort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzshortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzshortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezadmin/ezshort/EzshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezadmin.ezshort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzshortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
